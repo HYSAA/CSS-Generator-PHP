11a87dfa965bccf9151adbdaa47b3123f76f0cae
jahrixx <166481107+jahrixx@users.noreply.github.com>
2024-05-19 17:44:54 +0800

    mekus mekus

---
+++ SP_Finals_New/dummy.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS AND HTML GENERATOR</title>
+</head>
+<body>
+    <form action="main.php" method="post">
+        <button type="submit">Generate Files</button>
+        <button type="submit" name="action" value="delete">Delete Files</button>
+    </form>
+</body>
+</html>
